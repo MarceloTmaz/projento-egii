4001162f3106cf4d662f39a4d9fb7ef9af7461ec
MarceloTmaz <104047071+MarceloTmaz@users.noreply.github.com>
2024-01-05 09:57:51 -0300

    atualização do teste e de view

--- __tests__/acceptance/Tarefa.test.ts
+++ __tests__/acceptance/Tarefa.test.ts
@@ -13,16 +13,41 @@ describe('Tarefa', ()=>{
 
         await driver.findElement(By.id('criar')).click();    
 
-        let nomenomeusuario = await driver.findElement(By.id('Testar a aplicação'));
-        let novoemailusuario = await driver.findElement(By.id('Rodar os testes durante a aula'));
+        let nome = await driver.findElement(By.id('Testar a aplicação'));
+        let descricao = await driver.findElement(By.id('Rodar os testes durante a aula'));
         let importancia= await driver.findElement(By.id('Testar a aplicação3'))
 
-
-        console.log(importancia)
-
-        expect(await nomenomeusuario.getText()).toEqual('Testar a aplicação');
-        expect(await novoemailusuario.getText()).toEqual('Rodar os testes durante a aula');
+        expect(await nome.getText()).toEqual('Testar a aplicação');
+        expect(await descricao.getText()).toEqual('Rodar os testes durante a aula');
         expect(await importancia.getText()).toEqual('3');
+    });
+
+    it('deve permitir a criação de tarefas', async ()=>{
+        let driver = await new Builder().forBrowser('chrome').build();
+
+        await driver.get('http://localhost:3000/tarefa/listar');  
+        
+        await driver.findElement(By.id('alterar1')).click();
+        
+        await driver.findElement(By.id('nom2')).clear()
+        await driver.findElement(By.id('nom2')).sendKeys('Comprar bolo');
+        await driver.findElement(By.id('descricao2')).clear()
+        await driver.findElement(By.id('descricao2')).sendKeys('comprar bolo na padaria nosso pão');
+        await driver.findElement(By.id('impotancia2')).clear()
+        await driver.findElement(By.id('impotancia2')).sendKeys('2');
+        await driver.findElement(By.id('email2')).sendKeys('maria@gmail.com');
+
+        await driver.findElement(By.id('atualizar')).click();    
+
+        let nome = await driver.findElement(By.id('Comprar bolo'));
+        let descricao = await driver.findElement(By.id('comprar bolo na padaria nosso pão'));
+        let importancia= await driver.findElement(By.id('Comprar bolo1'))
+        let email= await driver.findElement(By.id('maria@gmail.com1'))
+
+        expect(await nome.getText()).toEqual('Comprar bolo');
+        expect(await descricao.getText()).toEqual('comprar bolo na padaria nosso pão');
+        expect(await importancia.getText()).toEqual('2');
+        expect(await email.getText()).toEqual('maria@gmail.com');
     });
     
 });
